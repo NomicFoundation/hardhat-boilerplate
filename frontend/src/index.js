b681d471a8869282d73dd10cd8728435cb61c474
Stinky Finance <stinkyzeroex@gmail.com>
2021-07-17 15:14:06 -0700

    Adding new image logo, and adding colors to css file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import { Dapp } from "./components/Dapp";
 
 // We import bootstrap here, but you can remove if you want
 import "bootstrap/dist/css/bootstrap.css";
+import "./stylesheets/Dapp.css";
 
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
