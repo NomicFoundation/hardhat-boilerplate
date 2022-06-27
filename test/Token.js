0928a835b157d25bf8cd7fadcca84662db1e3366
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-06-27 18:10:26 -0400

    test: wrap comments

--- test/Token.js
+++ test/Token.js
@@ -7,21 +7,21 @@
 const { expect } = require("chai");
 
 // We use `loadFixture` to share common setups (or fixtures) between tests.
-// Using this simplifies your tests and makes them run faster, by taking advantage
-// or Hardhat Network's snapshot functionality.
+// Using this simplifies your tests and makes them run faster, by taking
+// advantage or Hardhat Network's snapshot functionality.
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
-// `describe` is a Mocha function that allows you to organize your tests. 
-//  Having your tests organized makes debugging them easier.
-//  All Mocha functions are available in the global scope.
+// `describe` is a Mocha function that allows you to organize your tests.
+// Having your tests organized makes debugging them easier. All Mocha
+// functions are available in the global scope.
 
-// `describe` receives the name of a section of your test suite, and a callback.
-// The callback must define the tests of that section. This callback can't be
-// an async function.
+// `describe` receives the name of a section of your test suite, and a
+// callback. The callback must define the tests of that section. This callback
+// can't be an async function.
 describe("Token contract", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshopt in every test.
+  // We define a fixture to reuse the same setup in every test. We use
+  // loadFixture to run this setup once, snapshot that state, and reset Hardhat
+  // Network to that snapshopt in every test.
   async function deployTokenFixture() {
     // Get the ContractFactory and Signers here.
     const Token = await ethers.getContractFactory("Token");
@@ -46,13 +46,14 @@ describe("Token contract", function () {
 
     // If the callback function is async, Mocha will `await` it.
     it("Should set the right owner", async function () {
-      // We use loadFixture to setup our environment, and then assert that things went well
+      // We use loadFixture to setup our environment, and then assert that
+      // things went well
       const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
       // Expect receives a value, and wraps it in an assertion objet. These
       // objects have a lot of utility methods to assert values.
 
-      // This test expects the owner variable stored in the contract to be equal
-      // to our Signer's owner.
+      // This test expects the owner variable stored in the contract to be
+      // equal to our Signer's owner.
       expect(await hardhatToken.owner()).to.equal(owner.address);
     });
 
