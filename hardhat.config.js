5e359ab7b693d52f5dffa1d6e08ff3662cdc7e04
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-07-01 11:33:02 -0400

    Fix grammar in comment
    Co-authored-by: Patricio Palladino <email@patriciopalladino.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
-      chainId: 1337 // We set 1337 to make interacting with MetaMask, simpler
+      chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
     }
   }
 };
