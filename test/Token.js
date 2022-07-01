d78b46998a7e830197335b2ad6352c75c7aff2c0
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-07-01 11:34:14 -0400

    Tweak comment formatting
    Co-authored-by: Patricio Palladino <email@patriciopalladino.com>

--- test/Token.js
+++ test/Token.js
@@ -14,7 +14,7 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 // `describe` is a Mocha function that allows you to organize your tests.
 // Having your tests organized makes debugging them easier. All Mocha
 // functions are available in the global scope.
-
+//
 // `describe` receives the name of a section of your test suite, and a
 // callback. The callback must define the tests of that section. This callback
 // can't be an async function.
