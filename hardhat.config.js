2887ef3f3bc8ea0d0d4292bedfb8daa6d89addc3
Arian Hosseini <arianhosseini@outlook.com>
2022-12-29 23:08:19 +0330

    switched to solc version 0.8.17

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,10 @@ require("./tasks/faucet");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-  networks: {
-    hardhat: {
-      chainId: 1337 // We set 1337 to make interacting with MetaMask simpler
-    }
-  }
+	solidity: "0.8.17",
+	networks: {
+		hardhat: {
+			chainId: 1337, // We set 1337 to make interacting with MetaMask simpler
+		},
+	},
 };
