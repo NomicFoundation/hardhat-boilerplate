a1282cb5bd6cca3ad78467276a36e83b7eeaa79e
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2023-08-13 12:00:53 +0800

    bump hardhat conf

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { config as dotEnvConfig } from "dotenv";
-
 // To load all available tasks you should it via manual import.
 // Thus, we imported index.ts of the ./tasks module.
 import "./tasks";
 
 dotEnvConfig();
 
-
 const DEFAULT_GAS_PRICE = 100_000_000_000
-const ENV_TESTNET_PRIVATE_KEY = process.env?.["PRIVATE_KEY"] ? [process.env?.["PRIVATE_KEY"]] : undefined
-const GANACHE_PRIVATE_KEYS = ["0x8d69c158e4da48d8b4c17410149f5dc9d5e58a1f5c8b36e3aad3907234bb0810"]
+const PRIVATE_KEYS = process.env?.["PRIVATE_KEY"] ? [process.env?.["PRIVATE_KEY"]] : undefined
 
 
 const config: HardhatUserConfig = {
@@ -26,22 +23,21 @@ const config: HardhatUserConfig = {
   networks: {
     testnet: {
       url: process.env["TESTNET_RPC"] ?? "",
-      accounts: ENV_TESTNET_PRIVATE_KEY,
+      accounts: PRIVATE_KEYS,
       chainId: 80001,
       gasPrice: DEFAULT_GAS_PRICE,
     },
-    // One of the really available testnets with faucet coins (6 Aug 2023).
     testnetSepolia: {
       url: process.env["TESTNET_SEPOLIA_RPC"] ?? "",
-      accounts: ENV_TESTNET_PRIVATE_KEY,
+      accounts: PRIVATE_KEYS,
       chainId: 11155111,
       gasPrice: DEFAULT_GAS_PRICE,
     },
-    // For the Ganache testnet.
-    ganacheAws: {
-      url: process.env["GANACHE_AWS_RPC"] ?? "",
-      accounts: GANACHE_PRIVATE_KEYS,
-      chainId: 1337,
+    // For the Hardhat dev node.
+    hardhatDevNode: {
+      url: process.env["HARDHAT_DEV_NODE_RPC"] ?? "",
+      accounts: process.env?.["HARDHAT_DEV_NODE_PRIVATE_KEY"] ? [process.env?.["HARDHAT_DEV_NODE_PRIVATE_KEY"]] : undefined,
+      chainId: 31337,
       gasPrice: DEFAULT_GAS_PRICE,
     },
   }
